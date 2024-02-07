e5ef03ad6e3ddbab8300212e2fcb6be09267e2f4
khweza <khwezavuyo@gmail.com>
2024-02-07 12:42:14 +0200

    my first git class test

---
+++ Greenview-Systems_development/Python/4-main.py
@@ -0,0 +1,8 @@
+class Car:
+    """ Represents a caer in a car lot """
+    def __init__(Self,make,model,year,price_usd):
+        Self.make = make
+        self.model = model
+        self.year = year
+        self.price_usd = price_usd
+
